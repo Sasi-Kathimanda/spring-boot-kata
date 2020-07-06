31aa93429227399caf96fa5e2faca1aaa83833d6
Sasi Kathimanda <Sasi.Kathimanda@metapack.com>
2020-07-06 23:14:34 +0100

    first commit

---
+++ src/test/java/com/spring/boot/kata/SpringBootKataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.boot.kata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
