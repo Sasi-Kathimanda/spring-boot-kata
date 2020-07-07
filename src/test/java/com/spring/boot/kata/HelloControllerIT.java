a9eab6d2f39cb253e3bd4bbe31a685835f0a78c1
Sasi Kathimanda <Sasi.Kathimanda@metapack.com>
2020-07-07 21:35:13 +0100

    adding integration test for hello controller

---
+++ src/test/java/com/spring/boot/kata/HelloControllerIT.java
@@ -0,0 +1,35 @@
+package com.spring.boot.kata;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class HelloControllerIT {
+    @LocalServerPort
+    private int port;
+    private URL base;
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @BeforeEach
+    public void setUp() throws MalformedURLException {
+        this.base = new URL("http://localhost:"+port);
+    }
+
+    @Test
+    public void getHello() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(base.toString(), String.class);
+        assertThat(responseEntity.getBody()).isEqualTo("Hello Spring Boot!!");
+    }
+
+}
