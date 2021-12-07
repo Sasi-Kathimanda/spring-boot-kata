2449efba90fe64b7accbf0afaace8899f25f2708
Sasi Kathimanda <ksasikiranraja@gmail.com>
2021-12-07 22:31:33 +0000

    adding logging to the project

--- app/src/main/java/com/spring/boot/kata/HelloController.java
+++ app/src/main/java/com/spring/boot/kata/HelloController.java
@@ -1,12 +1,18 @@
 package com.spring.boot.kata;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.TimeUnit;
+
 @RestController
+@Slf4j
 public class HelloController {
     @RequestMapping
-    public String index(){
-        return  "Hello Spring Boot!!";
+    public String index() {
+        long unixTime = TimeUnit.MILLISECONDS.toSeconds(System.currentTimeMillis());
+        log.info("hitting home page from {}", unixTime);
+        return "Hello Spring Boot!!";
     }
 }
