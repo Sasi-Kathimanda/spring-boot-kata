9ba543db109b88c7c1f077deeb2577674056f226
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-08-16 20:51:36 +0100

    [SPRING-DI]- passing test to property injection using spring di

--- app/src/main/java/com/spring/boot/kata/controller/PropertyInjectedController.java
+++ app/src/main/java/com/spring/boot/kata/controller/PropertyInjectedController.java
@@ -1,8 +1,12 @@
 package com.spring.boot.kata.controller;
 
 import com.spring.boot.kata.service.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
+    @Autowired
     GreetingService greetingService;
 
     public String greet() {
