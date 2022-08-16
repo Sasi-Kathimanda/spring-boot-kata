1145b462388292c992aaa027bd5917d7ab854ac5
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-08-16 20:18:52 +0100

    [SPRING-DEPENDENCY-INJECTION]- adding failing IT test for NoSuchBeanDefinitionException: No bean named setterInjectedController

---
+++ app/src/test/java/com/spring/boot/kata/controller/SetterInjectedControllerITTest.java
@@ -0,0 +1,20 @@
+package com.spring.boot.kata.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class SetterInjectedControllerITTest {
+    @Autowired
+    ApplicationContext context;
+
+    @Test
+    void setterInjectionViaSpringBean() {
+        var controller = (SetterInjectedController) context.getBean("setterInjectedController");
+        assertEquals("Hello Hello", controller.greet());
+    }
+}
