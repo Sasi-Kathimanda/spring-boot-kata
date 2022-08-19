51323f5861e846e82369ef0dee6decbb89fed250
Sasi-Kathimanda <ksasikiranraja@gmail.com>
2022-08-19 23:26:03 +0100

    [SPRING-PROFILE]-adding enable if from spring test context

--- app/src/test/java/com/spring/boot/kata/controller/I18nControllerIT.java
+++ app/src/test/java/com/spring/boot/kata/controller/I18nControllerIT.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.EnabledIf;
 
 @SpringBootTest
 class I18nControllerIT {
@@ -13,6 +13,7 @@ class I18nControllerIT {
     ApplicationContext context;
 
     @Test
+    @EnabledIf(expression = "#{environment['spring.profiles.active'] == null}", loadContext = true)
     void shouldGiveENMessageWhenProfileSetAsEN() {
         var controller = (I18nController) context.getBean("i18nController");
         Assertions.assertEquals("Hello World in EN", controller.greet());
