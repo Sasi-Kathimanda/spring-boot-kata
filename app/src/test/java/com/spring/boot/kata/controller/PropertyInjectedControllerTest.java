0d11341ac50630590d12a98fb6fc7b0ac434dbf2
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-08-21 21:59:04 +0100

    [BAU]- removing wild card import

--- app/src/test/java/com/spring/boot/kata/controller/PropertyInjectedControllerTest.java
+++ app/src/test/java/com/spring/boot/kata/controller/PropertyInjectedControllerTest.java
@@ -4,7 +4,7 @@ import com.spring.boot.kata.service.GreetingServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PropertyInjectedControllerTest {
     PropertyInjectedController sut;
