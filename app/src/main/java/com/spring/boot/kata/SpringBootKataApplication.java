da02b8d944e8655a1f57d5032a51d2b30458db90
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-08-29 13:25:56 +0100

    [BAU]- removing unused import

--- app/src/main/java/com/spring/boot/kata/SpringBootKataApplication.java
+++ app/src/main/java/com/spring/boot/kata/SpringBootKataApplication.java
@@ -1,6 +1,5 @@
 package com.spring.boot.kata;
 
-import com.spring.boot.kata.controller.MyController;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
