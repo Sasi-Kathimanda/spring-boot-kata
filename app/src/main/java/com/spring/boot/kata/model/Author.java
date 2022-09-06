008a494ccc184f73ecbc4d9ee1613ce200684c44
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-09-06 07:29:44 +0100

    [JPA]-adding the equality and to string methods

--- app/src/main/java/com/spring/boot/kata/model/Author.java
+++ app/src/main/java/com/spring/boot/kata/model/Author.java
@@ -1,7 +1,5 @@
 package com.spring.boot.kata.model;
 
-import org.hibernate.annotations.ManyToAny;
-
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -46,5 +44,30 @@ public class Author {
 
     public void setBooks(Set<Book> books) {
         this.books = books;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Author author = (Author) o;
+
+        return id != null ? id.equals(author.id) : author.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Author{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", books=" + books +
+                '}';
     }
 }
