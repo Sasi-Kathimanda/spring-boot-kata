2de99128958d8f2e32d309e41fb547582b68c459
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-09-11 07:48:36 +0100

    [SPRING-REST] - adding book controller

---
+++ app/src/main/java/com/spring/boot/kata/controller/BookController.java
@@ -0,0 +1,21 @@
+package com.spring.boot.kata.controller;
+
+import com.spring.boot.kata.repositories.BookRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class BookController {
+    private BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
