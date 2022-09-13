0baf0c499732a06c5a49d36c846681f8b0efea0c
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-09-13 06:37:02 +0100

    [SPRING-TEST] - adding IT for book controller

---
+++ app/src/test/java/com/spring/boot/kata/controller/BookControllerIT.java
@@ -0,0 +1,34 @@
+package com.spring.boot.kata.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.ResponseEntity;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class BookControllerIT {
+    @LocalServerPort
+    String port;
+    URL base;
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @BeforeEach
+    void setUp() throws MalformedURLException {
+        this.base = new URL(String.format("http://localhost:%s/books", port));
+    }
+
+    @Test
+    void getBooks() {
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(base.toString(), String.class);
+        assertThat(responseEntity.getBody()).contains("The Alchemist");
+    }
+}
