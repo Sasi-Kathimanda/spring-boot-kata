7910d14065af7aaf7e0dfccb151a5d79b700e06a
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-09-14 06:44:53 +0100

    [SPRING-JPA] - using spring trasaction rather jee transaction

--- app/src/test/java/com/spring/boot/kata/controller/BookControllerTest.java
+++ app/src/test/java/com/spring/boot/kata/controller/BookControllerTest.java
@@ -7,8 +7,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
 
 import static org.hamcrest.core.StringContains.containsString;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
