1756442202e7ec080be7df3e55548031e6788041
Sasi Kathimanda <ksasikiranraja@gmail.com>
2022-09-15 06:50:45 +0100

    [SPRING] - adding chuck norris plugin

---
+++ jokes-app/src/main/java/service/JokesService.java
@@ -0,0 +1,5 @@
+package service;
+
+public interface JokesService {
+    String getJokes();
+}
---
+++ jokes-app/src/main/java/service/JokesServiceImpl.java
@@ -0,0 +1,8 @@
+package service;
+
+public class JokesServiceImpl implements JokesService {
+    @Override
+    public String getJokes() {
+        return null;
+    }
+}
